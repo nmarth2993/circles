6a30ca5821b009343e1b552442119b607ed0d8e5
nmarth2993 <nmarth2993@gmail.com>
2019-09-10 17:39:12 -0400

    Add: More directions, growing vs. constant

--- ColoredCircle.java
+++ ColoredCircle.java
@@ -1,37 +1,136 @@
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 
 public class ColoredCircle {
+
+	private boolean up;
+	private boolean down;
+	private boolean left;
+	private boolean right;
+
+	private static boolean growing;
+
 	private Ellipse2D circle;
 	private Color color;
-	private int tickSize;
-	
-	
-	public ColoredCircle(Ellipse2D c1, Color c2) {
+	private int sizeTick;
+	private int distanceTick;
+
+	public ColoredCircle() {
+
+	}
+
+	public ColoredCircle(Ellipse2D c1, Color c2, boolean up, boolean down, boolean left, boolean right) {
 		circle = c1;
 		color = c2;
-		tickSize = 2;
+		sizeTick = 1;
+		distanceTick = 2;
+		this.up = up;
+		this.down = down;
+		this.left = left;
+		this.right = right;
 	}
-	
+
+	public ColoredCircle(Ellipse2D c1, Color c2, int direction) {
+		circle = c1;
+		color = c2;
+		sizeTick = 1;
+		distanceTick = 2;
+
+		this.up = false;
+		this.down = false;
+		this.left = false;
+		this.right = false;
+
+		if (direction == 1) {
+			this.up = true;
+		} else if (direction == 2) {
+			this.down = true;
+		} else if (direction == 3) {
+			this.left = true;
+		} else if (direction == 4) {
+			this.right = true;
+		} else if (direction == 5) {
+			this.up = true;
+			this.left = true;
+		} else if (direction == 6) {
+			this.up = true;
+			this.right = true;
+		} else if (direction == 7) {
+			this.down = true;
+			this.left = true;
+		} else {
+			this.down = true;
+			this.right = true;
+		}
+	}
+
+	public static boolean isGrowing() {
+		return growing;
+	}
+
+	public static void setGrowing(boolean g) {
+		growing = g;
+	}
+
 	public Ellipse2D getCircle() {
 		return circle;
 	}
-	
+
 	public void setCircle(Ellipse2D e) {
 		circle = e;
 	}
-	
+
 	public Color getColor() {
 		return color;
 	}
-	
+
 	public void tick() {
-		setCircle(new Ellipse2D.Double(getCircle().getX() /*+ tickSize*/, getCircle().getY() /*+ tickSize*/, getCircle().getWidth() + tickSize, getCircle().getHeight() + tickSize));
+		/*
+		 * if (direction == TOP_LEFT) { setCircle(new
+		 * Ellipse2D.Double(getCircle().getX() - distanceTick, getCircle().getY() -
+		 * distanceTick, getCircle().getWidth() + sizeTick, getCircle().getHeight() +
+		 * sizeTick)); } else if (direction == TOP_RIGHT) { setCircle(new
+		 * Ellipse2D.Double(getCircle().getX() + distanceTick, getCircle().getY() -
+		 * distanceTick, getCircle().getWidth() + sizeTick, getCircle().getHeight() +
+		 * sizeTick)); } else if (direction == BOTTOM_LEFT) { setCircle(new
+		 * Ellipse2D.Double(getCircle().getX() - distanceTick, getCircle().getY() +
+		 * distanceTick, getCircle().getWidth() + sizeTick, getCircle().getHeight() +
+		 * sizeTick)); } else { setCircle(new Ellipse2D.Double(getCircle().getX() +
+		 * distanceTick, getCircle().getY() + distanceTick, getCircle().getWidth() +
+		 * sizeTick, getCircle().getHeight() + sizeTick)); }
+		 */
+//		System.out.println("(" + getCircle().getX() + ", " + getCircle().getY() + ")");
+		if (up) {
+			setCircle(new Ellipse2D.Double(getCircle().getX(), getCircle().getY() - distanceTick,
+					getCircle().getWidth(), getCircle().getHeight()));
+		}
+		if (down) {
+			setCircle(new Ellipse2D.Double(getCircle().getX(), getCircle().getY() + distanceTick,
+					getCircle().getWidth(), getCircle().getHeight()));
+		}
+		if (left) {
+			setCircle(new Ellipse2D.Double(getCircle().getX() - distanceTick, getCircle().getY(),
+					getCircle().getWidth(), getCircle().getHeight()));
+		}
+		if (right) {
+			setCircle(new Ellipse2D.Double(getCircle().getX() + distanceTick, getCircle().getY(),
+					getCircle().getWidth(), getCircle().getHeight()));
+		}
+		if (growing) {
+			setCircle(new Ellipse2D.Double(getCircle().getX(), getCircle().getY(), getCircle().getWidth() + sizeTick,
+					getCircle().getHeight() + sizeTick));
+		}
+//		circle.setFrame(
+//				new Rectangle((int) getCircle().getWidth() + sizeTick, (int) getCircle().getHeight() + sizeTick));
+
 	}
-	
-	public void setTickSize(int s) {
-		tickSize = s;
+
+	public void setSizeTick(int s) {
+		sizeTick = s;
 	}
-	
+
+	public void setDistanceTick(int d) {
+		distanceTick = d;
+	}
+
 }
--- Test.java
+++ Test.java
@@ -1,57 +1,83 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
+import java.awt.event.*;
 import java.awt.geom.Ellipse2D;
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 public class Test {
 	JFrame frame;
 	ArtPanel panel;
-	
+
 	public Test() {
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setPreferredSize(new Dimension(500, 500));
 		panel = new ArtPanel();
+		MouseCapture m = new MouseCapture();
+		panel.addMouseListener(m);
+		panel.addMouseMotionListener(m);
+		panel.createCircles();
 		frame.add(panel);
 		frame.setVisible(true);
 		frame.pack();
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
 			JFrame.setDefaultLookAndFeelDecorated(true);
 			new Test();
 		});
 	}
-	
+
 	class ArtPanel extends JPanel {
+		final static int circleCreationIntervalMin = 50;
+		final static int circleCreationIntervalMax = 300;
+
+		final static int MAX_SIZE = 30;
+
+		final static int diameter = 10;
+
 		ArrayList<ColoredCircle> circles;
 		Random r;
-		Color color;
-		
-		final static int circleCreationIntervalMin = 100;
-		final static int circleCreationIntervalMax = 1000;
-		
-		final static int size = 100;
-		final static int diameter = 10;
-		
+
+		Point mousePos;
+
+		int red;
+		int green;
+		int blue;
+
+		public Point getMousePos() {
+			return mousePos;
+		}
+
+		public void setMousePos(Point mousePos) {
+			this.mousePos = mousePos;
+		}
+
+		boolean mousePressed;
+
+		public boolean isMousePressed() {
+			return mousePressed;
+		}
+
+		public void setMousePressed(boolean mousePressed) {
+			this.mousePressed = mousePressed;
+		}
+
 		public ArtPanel() {
+			mousePos = new Point(250, 250);
+			mousePressed = false;
 			r = new Random();
 			circles = new ArrayList<ColoredCircle>();
-			color = new Color(0, 0, 0);
 			new Thread(() -> {
 				for (;;) {
-					for (ColoredCircle c : circles) {
+					ArrayList<ColoredCircle> cList = new ArrayList<ColoredCircle>(circles);
+					for (ColoredCircle c : cList) {
 						c.tick();
 					}
+
 					try {
 						Thread.sleep(10);
 					} catch (InterruptedException e) {
@@ -60,34 +86,107 @@ public class Test {
 					repaint();
 				}
 			}).start();
-			
 			new Thread(() -> {
+				final int gradientDelay = 5;
 				for (;;) {
-					color = new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256));
+
 				}
 			}).start();
-			
+		}
+
+		public void createCircles() {
 			new Thread(() -> {
 				for (;;) {
-					circles.add(new ColoredCircle(new Ellipse2D.Double(r.nextInt(size) + 1, r.nextInt(size) + 1, diameter, diameter), color));
-					try {
-						Thread.sleep(r.nextInt(circleCreationIntervalMax) + circleCreationIntervalMin);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+					circles.add(new ColoredCircle(
+							new Ellipse2D.Double(mousePos.getX(), mousePos.getY(), diameter, diameter),
+							new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)), r.nextInt(9) + 1));
+					if (isMousePressed()) {
+						/*
+						 * circles.add(new ColoredCircle( new Ellipse2D.Double(mousePos.getX() + 50,
+						 * mousePos.getY(), diameter, diameter), new Color(r.nextInt(256),
+						 * r.nextInt(256), r.nextInt(256)), r.nextInt(9) + 1));
+						 */
+						try {
+							Thread.sleep(3);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					} else {
+						try {
+							Thread.sleep(r.nextInt(circleCreationIntervalMax) + circleCreationIntervalMin);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
 					}
 				}
 			}).start();
-			
 		}
-		
+
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			Graphics2D g2d = (Graphics2D) g;
-			for (ColoredCircle c : circles) {
+			ArrayList<ColoredCircle> drawingShapes = new ArrayList<ColoredCircle>(circles);
+
+			/*
+			 * if (circles.size() > MAX_SIZE) { drawingShapes = new
+			 * ArrayList<ColoredCircle>( circles.subList(circles.size() - MAX_SIZE,
+			 * circles.size() - 1)); }
+			 */
+
+			panel.setBackground(new Color(red, green, blue));
+
+			for (ColoredCircle c : drawingShapes) {
 				g2d.setColor(c.getColor());
 				g2d.fill(c.getCircle());
 			}
 		}
+	}
+
+	class MouseCapture implements MouseMotionListener, MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			if (SwingUtilities.isRightMouseButton(e)) {
+				ColoredCircle.setGrowing((!ColoredCircle.isGrowing()));
+				return;
+			}
+			/*
+			 * if (SwingUtilities.isMiddleMouseButton(e)) {
+			 * panel.setMiddleMousePressed(true); }
+			 */
+
+			panel.setMousePressed(true);
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			panel.setMousePressed(false);
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			if (SwingUtilities.isLeftMouseButton(e)) {
+				mousePressed(e);
+			}
+			mouseMoved(e);
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			panel.setMousePos(e.getPoint());
+		}
+
 	}
 }
